248b61224b77c84becae78a92703e490310fdb91
Павловский Дмитрий Андреевич <dapavlovskii@domclick.ru>
2024-01-13 15:54:07 +0300

    add paralax

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,20 +1,23 @@
 import Layout from "@/components/layout/Layout";
-import dynamic from "next/dynamic";
 import styles from "./Home.module.css";
-
-const DynamicFooter = dynamic(
-  () => import("@/components/DynamicFooter/DynamicFooter"),
-  {
-    ssr: false,
-  }
-);
+import ParallaxDesc from "@/components/Parallax/ParallaxDesc";
+import ParallaxMobile from "@/components/Parallax/ParallaxMobile";
 
 export const Home = () => {
   return (
     <Layout title="Home">
-      <div className={styles.main}>partyhub.ru</div>
-
-      {/* <DynamicFooter /> */}
+      {/* <div className={styles.main}>partyhub.ru</div> */}
+      <div className={styles.desc}>
+        <ParallaxDesc />
+      </div>
+      <div className={styles.mobile}>
+        <ParallaxMobile />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.field}>
+          <div className={styles.mouse}></div>
+        </div>
+      </div>
     </Layout>
   );
 };
